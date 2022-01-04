4a4296679f295c000fa4806088579b97667070ff
Omar <omardraz94@gmail.com>
2022-01-03 20:12:58 -0800

    finished working on NFT project

--- contracts/SimpleCollectible.sol
+++ contracts/SimpleCollectible.sol
@@ -1,5 +1,4 @@
 // SPDX-License-Identifier: MIT
-
 pragma solidity 0.6.6;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
@@ -7,7 +6,7 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 contract SimpleCollectible is ERC721 {
     uint256 public tokenCounter;
 
-    constructor() public ERC721("Dogies", "DOG") {
+    constructor() public ERC721("Dogie", "DOG") {
         tokenCounter = 0;
     }
 
